cef9aa896e8245a77dfe249b004ef77de61b2448
Petr Hudeček <petrhudecek2010@gmail.com>
2020-07-18 23:11:09 +0200

    add auto-update version

--- Tools/Program.cs
+++ Tools/Program.cs
@@ -10,6 +10,12 @@ namespace Tools
         private static void Main(string[] args)
         {
             StringBuilder sb = new StringBuilder();
+            if (args.Length == 0)
+            {
+                Console.WriteLine("pot: Regenerate the .pot template file");
+                Console.WriteLine("updateversion: Alter the code files to set the next patch version.");
+                Console.WriteLine("package: Single command to create a signed publishable Android .apk a distributable Windows installer");
+            }
             if (args[0] == "pot")
             {
                 foreach (string filename in Directory.EnumerateFiles(".", "*.cs", SearchOption.AllDirectories))
@@ -21,6 +27,18 @@ namespace Tools
                     "-kT -kQSpeak:2 -kTn --from-code=utf-8 --package-name=NajdiCestuVen --package-version=2.0 -o Shared\\Texts\\NajdiCestuVen.pot --files-from=~allCSharpFiles.txt");
                 xGetText.WaitForExit();
                 Console.WriteLine(".pot creation exit code: " + xGetText.ExitCode);
+            }
+            else if (args[0] == "updateversion")
+            {
+                UpdateVersion.Execute();
+            }
+            else if (args[0] == "package")
+            {
+                UpdateVersion.Execute();
+            }
+            else
+            {
+               Console.WriteLine("Argument not recognized.");  
             }
         }
     }
---
+++ Tools/UpdateVersion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Tools
+{
+    public class UpdateVersion
+    {
+        public static void Execute()
+        {
+            string gameVersionFileName = "Build\\GameVersion.props";
+            XDocument xDocument = XDocument.Load(gameVersionFileName);
+            XElement xGameVersion = xDocument.Descendants("GameVersion").First();
+            Version currentVersion = Version.Parse(xGameVersion.Value);
+            Version updatedVersion = new Version(currentVersion.Major, currentVersion.Minor,
+                currentVersion.Revision + 1, currentVersion.Build);
+            
+            // Update main version file
+            xGameVersion.Value = updatedVersion.ToString(3);
+            xDocument.Save(gameVersionFileName);
+            
+            // Update android manifest
+            string manifestFileName = "Android\\Properties\\AndroidManifest.xml";
+            XDocument xManifest = XDocument.Load(manifestFileName);
+            XAttribute versionCode =
+                xManifest.Root.Attribute(XName.Get("versionCode", "http://schemas.android.com/apk/res/android"));
+            XAttribute versionName =
+                xManifest.Root.Attribute(XName.Get("versionName", "http://schemas.android.com/apk/res/android"));
+            versionCode.Value = (int.Parse(versionCode.Value) + 1).ToString();
+            versionName.Value = updatedVersion.ToString(3);
+            xManifest.Save(manifestFileName);
+        }
+    }
+}
